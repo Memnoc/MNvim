a5133d2dddc30c07237bc409070825bfeb68b143
Memnoc <smartdroidesign@gmail.com>
2026-01-19 21:59:39 +0000

    fully switched to Snacks and it's quite awesome

---
+++ lua/memnoc/plugins/snacks.lua
@@ -0,0 +1,240 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	keys = {
+		-- Explorer
+		{
+			"<leader>e",
+			function()
+				require("snacks").explorer()
+			end,
+			desc = "File Explorer",
+		},
+
+		-- Files
+		{
+			"<leader>ff",
+			function()
+				require("snacks").picker.files()
+			end,
+			desc = "Find Files",
+		},
+		{
+			"<leader>fr",
+			function()
+				require("snacks").picker.recent()
+			end,
+			desc = "Recent Files",
+		},
+		{
+			"<leader>fc",
+			function()
+				require("snacks").picker.files({ cwd = vim.fn.stdpath("config") })
+			end,
+			desc = "Config Files",
+		},
+		{
+			"<leader>fb",
+			function()
+				require("snacks").picker.buffers()
+			end,
+			desc = "Buffers",
+		},
+		{
+			"<leader><leader>",
+			function()
+				require("snacks").picker.buffers()
+			end,
+			desc = "Buffers",
+		},
+
+		-- Search
+		{
+			"<leader>sg",
+			function()
+				require("snacks").picker.grep()
+			end,
+			desc = "Grep",
+		},
+		{
+			"<leader>sw",
+			function()
+				require("snacks").picker.grep_word()
+			end,
+			desc = "Grep Word",
+		},
+		{
+			"<leader>sb",
+			function()
+				require("snacks").picker.lines()
+			end,
+			desc = "Buffer Lines",
+		},
+		{
+			"<leader>sh",
+			function()
+				require("snacks").picker.help()
+			end,
+			desc = "Help",
+		},
+		{
+			"<leader>sk",
+			function()
+				require("snacks").picker.keymaps()
+			end,
+			desc = "Keymaps",
+		},
+		{
+			"<leader>sd",
+			function()
+				require("snacks").picker.diagnostics()
+			end,
+			desc = "Diagnostics",
+		},
+		{
+			"<leader>ss",
+			function()
+				require("snacks").picker.lsp_symbols()
+			end,
+			desc = "LSP Symbols",
+		},
+		{
+			"<leader>sS",
+			function()
+				require("snacks").picker.lsp_workspace_symbols()
+			end,
+			desc = "Workspace Symbols",
+		},
+		{
+			"<leader>sr",
+			function()
+				require("snacks").picker.resume()
+			end,
+			desc = "Resume Last",
+		},
+		{
+			"<leader>s/",
+			function()
+				require("snacks").picker.grep_buffers()
+			end,
+			desc = "Grep Open Buffers",
+		},
+		{
+			"<leader>/",
+			function()
+				require("snacks").picker.lines()
+			end,
+			desc = "Buffer Lines",
+		},
+
+		-- Git
+		{
+			"<leader>gg",
+			function()
+				require("snacks").lazygit()
+			end,
+			desc = "Lazygit",
+		},
+		{
+			"<leader>gf",
+			function()
+				require("snacks").lazygit.log_file()
+			end,
+			desc = "File Log",
+		},
+		{
+			"<leader>gl",
+			function()
+				require("snacks").picker.git_log()
+			end,
+			desc = "Git Log",
+		},
+		{
+			"<leader>gs",
+			function()
+				require("snacks").picker.git_status()
+			end,
+			desc = "Git Status",
+		},
+		{
+			"<leader>gb",
+			function()
+				require("snacks").picker.git_branches()
+			end,
+			desc = "Git Branches",
+		},
+
+		-- LSP (also mapped in lspconfig, but available globally)
+		{
+			"gd",
+			function()
+				require("snacks").picker.lsp_definitions()
+			end,
+			desc = "Go to Definition",
+		},
+		{
+			"gr",
+			function()
+				require("snacks").picker.lsp_references()
+			end,
+			desc = "References",
+		},
+		{
+			"gI",
+			function()
+				require("snacks").picker.lsp_implementations()
+			end,
+			desc = "Implementations",
+		},
+		{
+			"gy",
+			function()
+				require("snacks").picker.lsp_type_definitions()
+			end,
+			desc = "Type Definitions",
+		},
+
+		-- Other
+		{
+			"<leader>un",
+			function()
+				require("snacks").notifier.hide()
+			end,
+			desc = "Dismiss Notifications",
+		},
+		{
+			"<leader>nh",
+			function()
+				require("snacks").notifier.show_history()
+			end,
+			desc = "Notification History",
+		},
+		{
+			"]]",
+			function()
+				require("snacks").words.jump(vim.v.count1)
+			end,
+			desc = "Next Reference",
+		},
+		{
+			"[[",
+			function()
+				require("snacks").words.jump(-vim.v.count1)
+			end,
+			desc = "Prev Reference",
+		},
+	},
+	opts = {
+		bigfile = { enabled = true },
+		explorer = { enabled = true, replace_netrw = true },
+		indent = { enabled = true },
+		input = { enabled = true },
+		notifier = { enabled = true },
+		picker = { enabled = true },
+		quickfile = { enabled = true },
+		scope = { enabled = true },
+		words = { enabled = true },
+		lazygit = { enabled = true },
+	},
+}
--- lua/memnoc/plugins/which-key.lua
+++ lua/memnoc/plugins/which-key.lua
@@ -1,13 +1,36 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 200
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    },
+	"folke/which-key.nvim",
+	event = "VimEnter",
+	config = function()
+		local wk = require("which-key")
+
+		wk.setup({
+			delay = 300,
+			icons = {
+				breadcrumb = "»",
+				separator = "➜",
+				group = "+",
+			},
+			win = {
+				border = "rounded",
+				padding = { 1, 2 },
+			},
+			layout = {
+				spacing = 3,
+			},
+		})
+
+		wk.add({
+			{ "<leader>b", group = "buffer", icon = "" },
+			{ "<leader>c", group = "code", icon = "" },
+			{ "<leader>f", group = "file/find", icon = "" },
+			{ "<leader>g", group = "git", icon = "" },
+			{ "<leader>p", group = "picker", icon = "" },
+			{ "<leader>q", group = "quit/session", icon = "" },
+			{ "<leader>s", group = "search", icon = "" },
+			{ "<leader>t", group = "toggle", icon = "" },
+			{ "<leader>u", group = "ui", icon = "" },
+			{ "<leader>x", group = "diagnostics", icon = "" },
+		})
+	end,
 }
