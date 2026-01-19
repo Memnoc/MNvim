b1427a38f7c11259d2c0a80f2dc3f7f35153f282
Matteo Stara <smartdroidesign@gmail.com>
2026-01-19 18:47:44 +0000

    added some fancy icons

--- lua/memnoc/plugins/colorscheme.lua
+++ lua/memnoc/plugins/colorscheme.lua
@@ -5,12 +5,22 @@ return {
 		config = function()
 			require("tokyonight").setup({
 				style = "night",
-				-- on_highlights = function(hl, c)
-				--    hl.NvimTreeGitNew = { fg = "#ff9e64" }
-				--    hl.NvimTreeGitDirty = { fg = "#ff9e64" }
-				--    hl.NvimTreeGitStaged = { fg = "#9ece6a" }
-				--    hl.NvimTreeGitRenamed = { fg = "#7aa2f7" }
-				--    hl.NvimTreeGitDeleted = { fg = "#f7768e" }
+				on_highlights = function(hl, c)
+					-- NvimTree (just in case)
+					-- hl.NvimTreeGitNew = { fg = "#ff9e64" }
+					-- hl.NvimTreeGitDirty = { fg = "#ff9e64" }
+					-- hl.NvimTreeGitStaged = { fg = "#9ece6a" }
+					-- hl.NvimTreeGitRenamed = { fg = "#7aa2f7" }
+					-- hl.NvimTreeGitDeleted = { fg = "#f7768e" }
+
+					-- NeoTree
+					hl.NeoTreeGitModified = { fg = "#ff9e64" }
+					hl.NeoTreeGitUntracked = { fg = "#ff9e64" }
+					hl.NeoTreeGitAdded = { fg = "#9ece6a" }
+					hl.NeoTreeGitDeleted = { fg = "#f7768e" }
+					hl.NeoTreeGitRenamed = { fg = "#7aa2f7" }
+					hl.NeoTreeGitStaged = { fg = "#9ece6a" }
+				end,
 			})
 			vim.cmd([[colorscheme tokyonight]])
 		end,
