bcd7f903e0adbd7b77b9354cfe8dbab8d446c0f0
Memnoc <smartdroidesign@gmail.com>
2026-01-20 06:57:12 +0000

    better lua line naming and little C compilation quality of life

--- lua/memnoc/core/autocommands.lua
+++ lua/memnoc/core/autocommands.lua
@@ -6,3 +6,35 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.hl.on_yank()
 	end,
 })
+
+-- C compilation
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "c",
+	callback = function()
+		vim.bo.makeprg = "gcc %:p -o %:p:r -Wall -Wextra"
+
+		vim.keymap.set("n", "<leader>cc", "<cmd>make<CR>", { buffer = true, desc = "Compile C" })
+
+		vim.keymap.set("n", "<leader>cr", function()
+			local out = vim.fn.expand("%:p:r")
+			vim.cmd('botright 15split | terminal bash -c "' .. out .. ' ; echo ; echo [Press ENTER to close] ; read"')
+		end, { buffer = true, desc = "Run C" })
+
+		vim.keymap.set("n", "<leader>cx", function()
+			local out = vim.fn.expand("%:p:r")
+			vim.cmd("make")
+			vim.cmd('botright 15split | terminal bash -c "' .. out .. ' ; echo ; echo [Press ENTER to close] ; read"')
+		end, { buffer = true, desc = "Compile and run C" })
+	end,
+})
+
+-- Rust compilation
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "rust",
+	callback = function()
+		vim.bo.makeprg = "cargo build"
+		vim.keymap.set("n", "<leader>cc", "<cmd>make<CR>", { buffer = true, desc = "Cargo build" })
+		vim.keymap.set("n", "<leader>cr", "<cmd>!cargo run<CR>", { buffer = true, desc = "Cargo run" })
+		vim.keymap.set("n", "<leader>ct", "<cmd>!cargo test<CR>", { buffer = true, desc = "Cargo test" })
+	end,
+})
