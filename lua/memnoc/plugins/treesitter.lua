bf7b24cdb579d7aa8088da366dca2f1508efdf83
Matteo Stara <smartdroidesign@gmail.com>
2026-01-20 11:12:27 +0000

    cleaned up treesitter file

--- lua/memnoc/plugins/treesitter.lua
+++ lua/memnoc/plugins/treesitter.lua
@@ -13,7 +13,7 @@ return {
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		config = function()
 			require("treesitter-autoinstall").setup({
-				highlight = true, -- auto-enable highlighting
+				highlight = true,
 				ignore = {
 					"snacks_notif",
 					"snacks_picker_preview",
@@ -42,54 +42,8 @@ return {
 		end,
 	},
 }
--- WARN: New API config from main
--- return {
--- branch = "main",
--- version = false,
--- 	-- "nvim-treesitter/nvim-treesitter",
--- 	build = ":TSUpdate",
--- 	config = function()
--- 		local ts = require("nvim-treesitter")
---
--- 		local ensure_installed = {
--- 			"lua",
--- 			"vim",
--- 			"vimdoc",
--- 			"c",
--- 			"rust",
--- 			"javascript",
--- 			"typescript",
--- 			"tsx",
--- 			"html",
--- 			"css",
--- 			"json",
--- 			"markdown",
--- 		}
---
--- 		local installed = ts.get_installed()
--- 		local to_install = vim.iter(ensure_installed)
--- 			:filter(function(p)
--- 				return not vim.tbl_contains(installed, p)
--- 			end)
--- 			:totable()
---
--- 		if #to_install > 0 then
--- 			ts.install(to_install)
--- 		end
---
--- 		vim.api.nvim_create_autocmd("FileType", {
--- 			pattern = ensure_installed,
--- 			callback = function()
--- 				vim.treesitter.start()
--- 				vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
--- 				vim.wo.foldmethod = "expr"
--- 				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
--- 			end,
--- 		})
--- 	end,
--- }
 
--- Stable API config
+-- WARN: OLD Stable API config
 -- return {
 -- 	"nvim-treesitter/nvim-treesitter",
 -- 	branch = "master",
