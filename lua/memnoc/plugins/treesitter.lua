76ae398f5955ce188c8196dc8bd1ca9c527b26d3
Matteo Stara <smartdroidesign@gmail.com>
2026-01-20 11:02:26 +0000

    migrated to new treesitter API

--- lua/memnoc/plugins/treesitter.lua
+++ lua/memnoc/plugins/treesitter.lua
@@ -1,7 +1,52 @@
--- New API config from main
--- WARN: not so stable
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		build = ":TSUpdate",
+		config = function()
+			-- main branch setup() is minimal - only install_dir option
+			require("nvim-treesitter").setup({})
+		end,
+	},
+	{
+		"mks-h/treesitter-autoinstall.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("treesitter-autoinstall").setup({
+				highlight = true, -- auto-enable highlighting
+				ignore = {
+					"snacks_notif",
+					"snacks_picker_preview",
+					"snacks_picker_input",
+					"snacks_picker_list",
+					"snacks_dashboard",
+					"snacks_terminal",
+					"snacks_layout_box",
+					"snacks_win_backdrop",
+					"cmp_docs",
+					"snacks_win",
+					"lazy",
+					"mason",
+					"TelescopePrompt",
+					"TelescopeResults",
+					"NvimTree",
+					"neo-tree",
+					"oil",
+					"help",
+					"checkhealth",
+					"qf",
+					"noice",
+					"toggleterm",
+				},
+			})
+		end,
+	},
+}
+-- WARN: New API config from main
 -- return {
--- 	"nvim-treesitter/nvim-treesitter",
+-- branch = "main",
+-- version = false,
+-- 	-- "nvim-treesitter/nvim-treesitter",
 -- 	build = ":TSUpdate",
 -- 	config = function()
 -- 		local ts = require("nvim-treesitter")
@@ -45,28 +90,28 @@
 -- }
 
 -- Stable API config
-return {
-	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"lua",
-				"vim",
-				"vimdoc",
-				"c",
-				"rust",
-				"javascript",
-				"typescript",
-				"tsx",
-				"html",
-				"css",
-				"json",
-			},
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
-}
+-- return {
+-- 	"nvim-treesitter/nvim-treesitter",
+-- 	branch = "master",
+-- 	build = ":TSUpdate",
+-- 	config = function()
+-- 		require("nvim-treesitter.configs").setup({
+-- 			ensure_installed = {
+-- 				"lua",
+-- 				"vim",
+-- 				"vimdoc",
+-- 				"c",
+-- 				"rust",
+-- 				"javascript",
+-- 				"typescript",
+-- 				"tsx",
+-- 				"html",
+-- 				"css",
+-- 				"json",
+-- 			},
+-- 			auto_install = true,
+-- 			highlight = { enable = true },
+-- 			indent = { enable = true },
+-- 		})
+-- 	end,
+-- }
