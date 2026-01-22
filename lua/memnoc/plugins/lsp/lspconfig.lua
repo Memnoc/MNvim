41062056faa711326d4b68058e0a643eae28aa9d
Memnoc <smartdroidesign@gmail.com>
2026-01-22 22:42:35 +0000

    fixed super annoying bug not displaying custom icons with lspconfig

--- lua/memnoc/plugins/lsp/lspconfig.lua
+++ lua/memnoc/plugins/lsp/lspconfig.lua
@@ -173,15 +173,16 @@ return {
 
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
-
 		vim.diagnostic.config({
 			virtual_text = true,
-			signs = true,
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "✘",
+					[vim.diagnostic.severity.WARN] = "▲",
+					[vim.diagnostic.severity.HINT] = "⚑",
+					[vim.diagnostic.severity.INFO] = "»",
+				},
+			},
 			underline = true,
 			update_in_insert = false,
 			severity_sort = true,
