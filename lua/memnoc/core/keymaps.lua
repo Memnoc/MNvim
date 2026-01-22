2ecfb8e1c99a9e03759e9e69deacfaa5249cb84e
Matteo Stara <smartdroidesign@gmail.com>
2026-01-22 17:09:26 +0000

    cleaning up some keymaps

--- lua/memnoc/core/keymaps.lua
+++ lua/memnoc/core/keymaps.lua
@@ -4,63 +4,18 @@ vim.g.mapleader = " "
 -- global variable
 local map = vim.keymap.set
 
--- General maps --
-
--- use jk to exit insert mode
--- map("i", "jk", "<ESC>", { desc = "Exit insert mode with Escape" })
-
--- delete single character without copying into registster
-map("n", "x", '"_x')
-
--- increment/decrement numbers
-map("n", "<leader>+", "<C-a>", { desc = "Increment number" })
-map("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
-
 -- vertical block selection
 map("n", "<leader>vb", "<C-v>G", { desc = "Block select to EOF" })
 
 -- window management
--- map("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
--- map("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 map("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make splits equal size
 map("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close the active split
-
--- Window navigation
--- map("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus left" })
--- map("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus right" })
--- map("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus down" })
--- map("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus up" })
-
--- tab management
--- map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- opens a new tab
--- map("n", "<leader>td", "<cmd>tabclose<CR>", { desc = "Close current tab" })
--- map("n", "<leader>tl", "<cmd>q<CR>", { desc = "Quit (closes last tab)" })
--- map("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) -- goes to next tab
--- map("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) -- goes to previous tab
--- map("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffert in new tab" }) -- moves current buffer to new tab
-
--- ctrl+s to save
--- map("n", "<C-s>", "<cmd>w<CR>", { desc = "Save file" })
--- map("i", "<C-s>", "<Esc><cmd>w<CR>", { desc = "Save file and exit insert" })
--- map("v", "<C-s>", "<Esc><cmd>w<CR>", { desc = "Save file and exit visual" })
-
--- Quit all (warns on unsaved)
--- map("n", "<leader>qq", "<cmd>qa<CR>", { desc = "Quit all" })
-
--- Save all and quit
--- map("n", "<leader>qq", "<cmd>wqa<CR>", { desc = "Save all and quit" })
 
 -- Force quit without saving
 map("n", "<leader>qQ", "<cmd>qa!<CR>", { desc = "Quit without saving" })
 
--- Clear search highlights
--- map("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlights" })
-
 -- Exit terminal mode
 map("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-
--- Lazy
--- map("n", "<leader>L", "<cmd>Lazy<CR>", { desc = "Open Lazy" })
 
 -- Better up/down (handles wrapped lines)
 map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
@@ -88,19 +43,19 @@ map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
 map("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
 map("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
 
--- Buffers
-map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
-map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
-map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
-map("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
-map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
-map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
-map("n", "<leader>bd", function()
-	require("snacks").bufdelete()
-end, { desc = "Delete Buffer" })
-map("n", "<leader>bo", function()
-	require("snacks").bufdelete.other()
-end, { desc = "Delete Other Buffers" })
+-- Buffers - defined in bufferline.lua
+-- map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+-- map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+-- map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+-- map("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+-- map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+-- map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+-- map("n", "<leader>bd", function()
+-- 	require("snacks").bufdelete()
+-- end, { desc = "Delete Buffer" })
+-- map("n", "<leader>bo", function()
+-- 	require("snacks").bufdelete.other()
+-- end, { desc = "Delete Other Buffers" })
 map("n", "<leader>bD", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
 
 -- Clear search on escape
@@ -150,92 +105,31 @@ map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
 -- New file
 map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
 
--- Quickfix / Location list
-map("n", "<leader>xl", function()
-	local win = vim.fn.getloclist(0, { winid = 0 }).winid
-	if win ~= 0 then
-		vim.cmd.lclose()
-	else
-		vim.cmd.lopen()
-	end
-end, { desc = "Toggle Location List" })
-
-map("n", "<leader>xq", function()
-	local win = vim.fn.getqflist({ winid = 0 }).winid
-	if win ~= 0 then
-		vim.cmd.cclose()
-	else
-		vim.cmd.copen()
-	end
-end, { desc = "Toggle Quickfix List" })
-
-map("n", "[q", vim.cmd.cprev, { desc = "Previous Quickfix" })
-map("n", "]q", vim.cmd.cnext, { desc = "Next Quickfix" })
-
--- Diagnostic navigation
--- map("n", "]d", function()
--- 	vim.diagnostic.goto_next({ float = true })
--- end, { desc = "Next Diagnostic" })
--- map("n", "[d", function()
--- 	vim.diagnostic.goto_prev({ float = true })
--- end, { desc = "Prev Diagnostic" })
--- map("n", "]e", function()
--- 	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR, float = true })
--- end, { desc = "Next Error" })
--- map("n", "[e", function()
--- 	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR, float = true })
--- end, { desc = "Prev Error" })
--- map("n", "]w", function()
--- 	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN, float = true })
--- end, { desc = "Next Warning" })
--- map("n", "[w", function()
--- 	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN, float = true })
--- end, { desc = "Prev Warning" })
-
--- Toggle options
--- map("n", "<leader>us", function()
--- 	vim.opt.spell = not vim.opt.spell:get()
--- end, { desc = "Toggle Spelling" })
--- map("n", "<leader>uw", function()
--- 	vim.opt.wrap = not vim.opt.wrap:get()
--- end, { desc = "Toggle Wrap" })
--- map("n", "<leader>uL", function()
--- 	vim.opt.relativenumber = not vim.opt.relativenumber:get()
--- end, { desc = "Toggle Relative Number" })
--- map("n", "<leader>ul", function()
--- 	vim.opt.number = not vim.opt.number:get()
--- end, { desc = "Toggle Line Number" })
-
 -- Quit
 map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
 
 -- Inspect
 map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
 
--- Terminal
+-- Snacks Terminal
 map("n", "<leader>ft", function()
 	require("snacks").terminal()
 end, { desc = "Terminal" })
+
 map({ "n", "t" }, "<C-/>", function()
 	require("snacks").terminal.toggle()
 end, { desc = "Toggle Terminal" })
 
 -- Windows
-map("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
-map("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
+map("n", "<leader>|", "<C-W>s", { desc = "Split Window Below", remap = true })
+map("n", "<leader>/", "<C-W>v", { desc = "Split Window Right", remap = true })
 map("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
-map("n", "<leader>wm", function()
-	require("snacks").toggle.zoom()
-end, { desc = "Toggle Zoom" })
-map("n", "<leader>uz", function()
-	require("snacks").toggle.zen()
-end, { desc = "Toggle Zen Mode" })
 
--- Tabs
-map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
-map("n", "<leader><tab>o", "<cmd>tabonly<cr>", { desc = "Close Other Tabs" })
-map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
-map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
-map("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
-map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+-- Tabs defined in bufferline
+-- map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
+-- map("n", "<leader><tab>o", "<cmd>tabonly<cr>", { desc = "Close Other Tabs" })
+-- map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
+-- map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
+-- map("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+-- map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+-- map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
--- lua/memnoc/plugins/which-key.lua
+++ lua/memnoc/plugins/which-key.lua
@@ -21,17 +21,17 @@ return {
 		})
 
 		wk.add({
-			{ "<leader>b", group = "buffer", icon = "" },
-			{ "<leader>c", group = "code", icon = "" },
-			{ "<leader>f", group = "file/find", icon = "" },
-			{ "<leader>g", group = "git", icon = "" },
-			{ "<leader>q", group = "quit/session", icon = "" },
-			{ "<leader>s", group = "search", icon = "" },
-			{ "<leader>t", group = "toggle", icon = "" },
-			{ "<leader>u", group = "ui", icon = "" },
-			{ "<leader>w", group = "window", icon = "" },
-			{ "<leader>x", group = "diagnostics", icon = "" },
-			{ "<leader><tab>", group = "tabs", icon = "" },
+			{ "<leader>b", group = "buffer", icon = "󰖲 " },
+			{ "<leader>c", group = "code", icon = "󰈮 " },
+			{ "<leader>f", group = "file/find", icon = "󰮗 " },
+			-- { "<leader>g", group = "git", icon = "" },
+			{ "<leader>q", group = "quit/session", icon = "󰗼 " },
+			{ "<leader>s", group = "search", icon = " " },
+			{ "<leader>t", group = "toggle", icon = " " },
+			{ "<leader>u", group = "ui", icon = " " },
+			{ "<leader>w", group = "window", icon = " " },
+			{ "<leader>x", group = "diagnostics", icon = "󰩂 " },
+			-- { "<leader><tab>", group = "tabs", icon = " " },
 		})
 	end,
 }
