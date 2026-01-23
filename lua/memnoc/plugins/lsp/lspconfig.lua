7707808f6400df5a8ca0451e3b82dbceed82a9bd
Memnoc <smartdroidesign@gmail.com>
2026-01-23 08:07:57 +0000

    re-added git symbols fix

--- lua/memnoc/plugins/lsp/lspconfig.lua
+++ lua/memnoc/plugins/lsp/lspconfig.lua
@@ -92,11 +92,24 @@ return {
 
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
+		vim.diagnostic.config({
+			virtual_text = true,
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.WARN] = "",
+					[vim.diagnostic.severity.HINT] = "",
+					[vim.diagnostic.severity.INFO] = "",
+				},
+			},
+			underline = true,
+			update_in_insert = false,
+			severity_sort = true,
+			float = {
+				border = "rounded",
+				source = "if_many",
+			},
+		})
 
 		vim.diagnostic.config({
 			virtual_text = true,
