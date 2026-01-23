566cabcbf7e2560eb2420d903633470566d1bf24
Memnoc <smartdroidesign@gmail.com>
2026-01-23 07:54:25 +0000

    Add dap, flash, harpoon, surround, undotree plugins

---
+++ lua/memnoc/plugins/harpoon.lua
@@ -0,0 +1,31 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = {
+		{ "<leader>ha", function() require("harpoon"):list():add() end, desc = "Add file" },
+		{ "<leader>hh", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, desc = "Quick menu" },
+		{ "<leader>h1", function() require("harpoon"):list():select(1) end, desc = "File 1" },
+		{ "<leader>h2", function() require("harpoon"):list():select(2) end, desc = "File 2" },
+		{ "<leader>h3", function() require("harpoon"):list():select(3) end, desc = "File 3" },
+		{ "<leader>h4", function() require("harpoon"):list():select(4) end, desc = "File 4" },
+		{ "<leader>h5", function() require("harpoon"):list():select(5) end, desc = "File 5" },
+		{ "<leader>hp", function() require("harpoon"):list():prev() end, desc = "Previous file" },
+		{ "<leader>hn", function() require("harpoon"):list():next() end, desc = "Next file" },
+		-- Quick access with Alt+number
+		{ "<A-1>", function() require("harpoon"):list():select(1) end, desc = "Harpoon file 1" },
+		{ "<A-2>", function() require("harpoon"):list():select(2) end, desc = "Harpoon file 2" },
+		{ "<A-3>", function() require("harpoon"):list():select(3) end, desc = "Harpoon file 3" },
+		{ "<A-4>", function() require("harpoon"):list():select(4) end, desc = "Harpoon file 4" },
+		{ "<A-5>", function() require("harpoon"):list():select(5) end, desc = "Harpoon file 5" },
+	},
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon:setup({
+			settings = {
+				save_on_toggle = true,
+				sync_on_ui_close = true,
+			},
+		})
+	end,
+}
